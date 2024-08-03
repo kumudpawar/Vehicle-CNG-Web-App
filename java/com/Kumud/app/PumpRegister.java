fd80a3464c832be1ec8f08b98854ba0e08dd12e0
kumudpawar <119783414+kumudpawar@users.noreply.github.com>
2024-08-03 16:46:23 +0530

    Update PumpRegister.java

--- java/com/Kumud/app/PumpRegister.java
+++ java/com/Kumud/app/PumpRegister.java
@@ -6,17 +6,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.Swaraj.data.ConnectJDBC;
 public class PumpRegister extends HttpServlet 
 {
 	private static final long serialVersionUID = 1L;
-     public PumpRegister() {
+     public PumpRegister() 
+	{
         super();
-    }
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   	 }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
 		doGet(request, response);
 		int id = 0;
 		String name = request.getParameter("pname");
