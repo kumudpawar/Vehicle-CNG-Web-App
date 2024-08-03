827b7d695cc220c127b8272c31848cc10f505cee
kumudpawar <119783414+kumudpawar@users.noreply.github.com>
2024-08-03 16:48:05 +0530

    Update UserLogin.java

--- java/com/Kumud/app/UserLogin.java
+++ java/com/Kumud/app/UserLogin.java
@@ -7,8 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.Swaraj.data.ConnectJDBC;
-import com.Swaraj.data.GetSet;
+
 public class UserLogin extends HttpServlet 
 {
 	private static final long serialVersionUID = 1L;
