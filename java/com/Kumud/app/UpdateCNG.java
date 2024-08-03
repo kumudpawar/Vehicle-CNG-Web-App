f1d351ee77951e3691b1ffbb6e02b6d2eb780b00
kumudpawar <119783414+kumudpawar@users.noreply.github.com>
2024-08-03 16:47:42 +0530

    Update UpdateCNG.java

--- java/com/Kumud/app/UpdateCNG.java
+++ java/com/Kumud/app/UpdateCNG.java
@@ -6,16 +6,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.Swaraj.data.ConnectJDBC;
-public class UpdateCNG extends HttpServlet {
+public class UpdateCNG extends HttpServlet 
+{
 	private static final long serialVersionUID = 1L;
-    public UpdateCNG() {
+    public UpdateCNG() 
+	{
         super();
-    }
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   	 }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
 		doGet(request, response);
 		String mono = request.getParameter("mobile");
 		String upass = request.getParameter("password");
