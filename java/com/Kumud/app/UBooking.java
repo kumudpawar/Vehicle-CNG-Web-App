8aa0aa2e867fd7b7afd45c0ab13a13e8adc13359
kumudpawar <119783414+kumudpawar@users.noreply.github.com>
2024-08-03 16:47:05 +0530

    Update UBooking.java

--- java/com/Kumud/app/UBooking.java
+++ java/com/Kumud/app/UBooking.java
@@ -9,11 +9,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class UBooking extends HttpServlet {
+public class UBooking extends HttpServlet 
+{
 	private static final long serialVersionUID = 1L;
-       public UBooking() {
+       public UBooking() 
+	{
         super();
-    }
+   	 }
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
